8b4bb67ab73ac9b9e8bd2ab99adf0ec7256a1876
pratikgl <pratikgoyal2712@gmail.com>
2021-06-07 14:04:38 +0530

    App.js updated for signup and signin route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import Home from "./Components/Home";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import SignUp from './Components/SignUp';
+import SignIn from './Components/SignIn';
+import { AuthProvider } from './Contexts/AuthContext'
 
 function App() {
   return (
-    <div>
-      <Home></Home>
-    </div>
+    <AuthProvider>
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/signup' component={SignUp} />
+          <Route path='/signin' component={SignIn} />
+        </Switch>
+      </Router>
+    </AuthProvider>
   );
 }
 
