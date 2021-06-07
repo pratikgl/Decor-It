f158917ec01087398d5b73779da67f49cf3e5373
pratikgl <pratikgoyal2712@gmail.com>
2021-06-07 20:00:35 +0530

    modified Header.js, added logout

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import PersonIcon from '@material-ui/icons/Person';
 import logo from './../Assets/img/logo.png';
 import Image from 'react-bootstrap/Image'
-import { Container, Typography } from '@material-ui/core';
+import { Button, Container, Typography } from '@material-ui/core';
+import { useAuth } from '../Contexts/AuthContext';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import { useHistory } from 'react-router';
 
 const useStyles = makeStyles((theme) => ({
   toolbar: {
@@ -16,36 +18,71 @@ const useStyles = makeStyles((theme) => ({
   logo: {
     marginTop: 6,
     height: 50,
+  },
+  button: {
+    textTransform: 'none',
+    marginRight: 10,
   }
 }));
 
 export default function Header(props) {
   const classes = useStyles();
+
+  const [error, setError] = useState('')
+  const { currentUser, logout } = useAuth();
+  const history = useHistory()
+
+  async function handleLogout() {
+    setError('')
+
+    try {
+      await logout()
+      history.pushState('/')
+    } catch {
+      setError('Failed to log out')
+    }
+  }
+
   return (
     <React.Fragment>
       <Container maxWidth="lg" className={classes.toolbar} >
         <Toolbar>
-          {/* logo */}
+          {/* Decor It logo */}
           <Image src={logo} alt={'logo'} className={classes.logo} />
 
           {/* empty div */}
           <div style={{ flex: 1 }}></div>
 
           {/* Profile : Login */}
-          <IconButton>
-            <PersonIcon style={{ marginRight: 2 }} />
+          <Button
+            color='primary'
+            className={classes.button}
+            href={currentUser ? '/update_profile' : 'signin'}
+          >
+            <PersonIcon style={{ marginRight: 3 }} />
             <Typography >
-              User
+              {currentUser ? currentUser.email : 'Sign In'}
             </Typography>
-          </IconButton>
+          </Button>
 
           {/* Bucket */}
-          <IconButton>
-            <ShoppingCartIcon style={{ marginRight: 2 }} />
+          <Button color='primary' className={classes.button}>
+            <ShoppingCartIcon style={{ marginRight: 3 }} />
             <Typography>
               Cart
             </Typography>
-          </IconButton>
+          </Button>
+
+          {
+            /* Log out button only is currently logged in */
+            currentUser &&
+            <Button color='primary' className={classes.button} onClick={handleLogout}>
+              <ExitToAppIcon style={{ marginRight: 3 }} />
+              <Typography>
+                Log Out
+            </Typography>
+            </Button>
+          }
         </Toolbar>
       </Container>
     </React.Fragment>
