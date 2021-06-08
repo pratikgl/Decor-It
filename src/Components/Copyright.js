6326a974efb31ed62cc4e293a29c408b7b624df0
pratikgl <pratikgoyal2712@gmail.com>
2021-06-08 15:18:33 +0530

    separate component for Copyright created

---
+++ src/Components/Copyright.js
@@ -0,0 +1,15 @@
+import { Link, Typography } from '@material-ui/core';
+import React from 'react'
+
+export default function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="/">
+        Decor It
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
