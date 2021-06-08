6f12531c7e5200a56c1f1dd2c48385e0ef04b522
pratikgl <pratikgoyal2712@gmail.com>
2021-06-08 11:31:57 +0530

    Remember Me check box removed

--- src/Components/SignIn.js
+++ src/Components/SignIn.js
@@ -3,8 +3,6 @@ import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { TextField } from '@material-ui/core'
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
@@ -31,7 +29,7 @@ function Copyright() {
 }
 const useStyles = makeStyles((theme) => ({
   paper: {
-    marginTop: theme.spacing(8),
+    marginTop: theme.spacing(13),
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
@@ -119,10 +117,6 @@ export default function SignIn() {
             type="password"
             id="password"
             autoComplete="current-password"
-          />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
           />
           <Button
             type="submit"
