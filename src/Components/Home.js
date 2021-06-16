6864a29dc684e201c096bfd42969bc9ab431f041
pratikgl <pratikgoyal2712@gmail.com>
2021-06-16 15:04:59 +0530

    backdrop animation added

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,8 +5,20 @@ import { useAuth } from '../Contexts/AuthContext';
 import HomeItems from './HomeItems';
 import HomeDashboard from './HomeDashboard';
 import HomeFooter from './HomeFooter';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Backdrop from '@material-ui/core/Backdrop';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: '#fff',
+  },
+}));
 
 export default function Home() {
+  const classes = useStyles();
+
   const { getItems } = useAuth();
   const [items, setItems] = useState()
 
@@ -24,7 +36,11 @@ export default function Home() {
       <Header />
       <main>
         <HomeDashboard />
-        {items && <HomeItems items={items} />}
+        {items ?
+          <HomeItems items={items} /> :
+          <Backdrop className={classes.backdrop} open={true}>
+            <CircularProgress color="inherit" />
+          </Backdrop>}
       </main>
       <HomeFooter />
     </React.Fragment>
